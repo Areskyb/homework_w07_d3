a4891770bd67e593f0f5df65b02900aa619e5367
Aresky Berkane <areskyberkane@gmail.com>
2019-11-26 20:55:17 +0000

    finished form

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 import Vue from 'vue';
 
-document.addEventLiestener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",()=>{
   new Vue({
     el:"#app",
     data:{
-      
+      rates: '',
+      asked: 0
     },
     mounted(){
       this.fetchCoins()
     },
     methods: {
       fetchCoins: function(){
-        
+        const request = fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => {
+          this.rates = (data.rates)  
+        })
       }
     }
   })
